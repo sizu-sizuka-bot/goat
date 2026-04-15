4188194499dfcb829d0482fd9ffc6ad51b5209aa
SIZUKA <piccisizuka0@gmail.com>
2026-04-15 23:37:57 +0600

    Create cr.js

---
+++ scripts/cmds/cr.js
@@ -0,0 +1,48 @@
+module.exports = {
+config: {
+name: "cr",
+version: "2.0",
+author: "MR_FARHAN",
+countDown: 5,
+role: 0,
+shortDescription: "Create group with mention",
+longDescription: "Create messenger group using mention",
+category: "group",
+guide: "{pn} <group name> @mention"
+},
+
+onStart: async function ({ api, event, args }) {
+
+const mentions = Object.keys(event.mentions);
+
+if (mentions.length == 0) {
+return api.sendMessage("⚠️ Please mention users\nExample:\n-cr Pookie Group @user", event.threadID);
+}
+
+const groupName = args.join(" ").replace(/@\S+/g, "").trim();
+
+if (!groupName) {
+return api.sendMessage("⚠️ Give group name", event.threadID);
+}
+
+const members = [...mentions, event.senderID];
+
+api.createNewGroup(members, groupName, (err, info) => {
+
+if (err) return api.sendMessage("❌ Group create failed!", event.threadID);
+
+api.sendMessage(
+`🎉 GROUP CREATED
+
+👑 Owner: ${event.senderID}
+👥 Members: ${members.length}
+📌 Name: ${groupName}
+
+♡┋ 《𓆩𝐒𝐈𝐙𝐔𝐊𝐀𓆪》ᥫ᭡🎀🙂`,
+info.threadID
+);
+
+});
+
+}
+};
