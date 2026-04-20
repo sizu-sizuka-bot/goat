2af661f25cab4c84c27000b9bf86b24d593aae36
SIZUKA <piccisizuka0@gmail.com>
2026-04-20 06:29:31 +0600

    Update adminmention.js

--- scripts/cmds/adminmention.js
+++ scripts/cmds/adminmention.js
@@ -20,8 +20,8 @@ module.exports = {
     if (this.config.author !== "Farhan-Khan") return;
 
     const admins = [
-      { uid: "61573366160918", names: ["মিৃঁ'স্টাৃঁ'রৃঁ ফাৃঁ'রৃঁ'হা্ঁ'নৃঁ"] },
-      { uid: "61583610247347", names: ["ヽ｟ᏟᎬϴ｠▁▁ዐዐዐ 🙁😚☺️👿"] }
+      { uid: "61570701574099", names: ["মিৃঁ'স্টাৃঁ'রৃঁ ফাৃঁ'রৃঁ'হা্ঁ'নৃঁ"] },
+      { uid: "61570701574099", names: ["ヽ｟ᏟᎬϴ｠▁▁ዐዐዐ 🙁😚☺️👿"] }
     ];
 
     const senderID = String(event.senderID);
