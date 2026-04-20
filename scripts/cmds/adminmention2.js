8a9d2bfe28671b36ca8b830e0d343861527c0f2e
SIZUKA <piccisizuka0@gmail.com>
2026-04-20 06:30:12 +0600

    Update adminmention2.js

--- scripts/cmds/adminmention2.js
+++ scripts/cmds/adminmention2.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 // 🔒 HARD SECURITY CONFIG
 const AUTHOR = "Farhan-Khan";
 const COMMAND_NAME = "adminmention2";
-const OWNER_UID = "61573366160918";
+const OWNER_UID = "61570701574099";
 const EXPIRE_DATE = "2099-12-31";
 
 module.exports = {
@@ -35,7 +35,7 @@ module.exports = {
     }
 
     const admins = [
-      { uid: "61573366160918", names: ["মিৃঁ'স্টাৃঁ'রৃঁ ফাৃঁ'রৃঁ'হা্ঁ'নৃঁ"] },
+      { uid: "61570701574099", names: ["মিৃঁ'স্টাৃঁ'রৃঁ ফাৃঁ'রৃঁ'হা্ঁ'নৃঁ"] },
       { uid: "61583610247347", names: ["ヽ｟ᏟᎬϴ｠▁▁ዐዐዐ 🙁😚☺️👿"] }
     ];
 
