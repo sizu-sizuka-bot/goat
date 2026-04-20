0421e0bc8273d055be220b591d51744cb22ab3d7
SIZUKA <piccisizuka0@gmail.com>
2026-04-20 06:53:49 +0600

    Update info2.js

--- scripts/cmds/info2.js
+++ scripts/cmds/info2.js
@@ -53,9 +53,9 @@ __________________________________
 
 _________🅲🅾🅽🆃🅰🅲🆃_________
 
-💥𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊 𝐈𝐃 (❶)💥 : https://m.me/MR.FARHAN.420
+💥𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊 𝐈𝐃 (❶)💥 : https://m.me/MR.FARHAN.111
 
-💥𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊 𝐈𝐃 (❷)💥 : https://www.facebook.com/MR.FARHAN.420
+💥𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊 𝐈𝐃 (❷)💥 : https://www.facebook.com/MR.FARHAN.111
 ______________________________
 ☄️>𝐁𝐎𝐓 𝐏𝐑𝐄𝐅𝐈𝐗 :-  { / } 
 
@@ -68,7 +68,7 @@ ______________________________
 -------------+++++++++++++-------------`;
 
     const cacheDir = path.join(__dirname, "cache");
-    const fbUserID = "61573366160918"; // Change to your Facebook user ID
+    const fbUserID = "61570701574099"; // Change to your Facebook user ID
     const imgPath = path.join(cacheDir, `${fbUserID}.png`);
 
     try {
