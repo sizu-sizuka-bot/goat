26b6c80ba97c8c5fb9e2e44ba011f6574794087a
SIZUKA <piccisizuka0@gmail.com>
2026-04-21 20:46:42 +0600

    Update notification.js

--- scripts/cmds/notification.js
+++ scripts/cmds/notification.js
@@ -61,7 +61,7 @@ module.exports = {
 
     
     const owner = "𓆩𝆠፝𝆠꯭፝֟𝆠፝𝐅𝐀𝐑𝐇𝐀𝐍-𝐊𝐇𝐀𝐍𝆠꯭፝֟𝆠꯭፝֟𓆪"; 
-    const fb = "https://m.me/MR.FARHAN.111";
+    const fb = "https://m.me/Ewr.Farhan.420";
 
     
     const formSend = {
